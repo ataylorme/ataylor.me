969951bca512cfa03d08557ac16daad811d3a78c
ataylorme <andrew@ataylor.me>
2017-12-19 11:27:51 -0800

    Add build and deploy step for Circle CI

---
+++ .circleci/build-gatsby.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install dependencies with npm
+NPM_FOUND=`which npm`
+if [ ! -x "$NPM_FOUND" ]; then
+	echo -e "\nError: executable npm not found on path"
+	exit 1
+fi
+if [ ! -f ./package.json ]; then
+	echo -e "\nError: package.json not found"
+	exit 1
+fi
+
+npm install
+
+# Build site with gatsby
+GATSBY_FOUND=`which gatsby`
+if [ ! -x "$GATSBY_FOUND" ]; then
+	echo -e "\nError: executable gatsby not found on path"
+	exit 1
+fi
+if [ ! -f ./gatsby-config.js ]; then
+	echo -e "\nError: gatsby-config.js not found"
+	exit 1
+fi
+
+gatsby build --prefix-paths
---
+++ .circleci/deploy-to-s3.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Bail if missing the access key
+if [ -z "$S3_ACCESS_KEY" ]; then
+ echo -e "\n S3_ACCESS_KEY is not defined. Please setup environment variables."
+ exit 1
+fi
+
+# Bail if missing the access secret
+if [ -z "$S3_ACCESS_SECRET" ]; then
+ echo -e "\n S3_ACCESS_SECRET is not defined. Please setup environment variables."
+ exit 1
+fi
+
+# Upload media to S3
+echo -e "\n-> Uploading media in public/static to S3..."
+s3cmd \
+--access_key=$S3_ACCESS_KEY \
+--secret_key=$S3_ACCESS_SECRET \
+sync \
+--acl-public \
+--delete-removed \
+--guess-mime-type \
+--no-mime-magic \
+--cf-invalidate \
+--exclude='.DS_Store' \
+--exclude='CNAME' \
+--add-header="Cache-Control: max-age=31556926"  \
+public/static/ s3://media.ataylor.me/
+
+# Upload site to S3
+echo -e "\n-> Uploading site, excluding media and CSS/JS, to S3..."
+s3cmd \
+--access_key=$S3_ACCESS_KEY \
+--secret_key=$S3_ACCESS_SECRET \
+sync \
+--acl-public \
+--delete-removed \
+--no-mime-magic \
+--guess-mime-type \
+--cf-invalidate \
+--exclude='.DS_Store' \
+--exclude='CNAME' \
+--exclude='static/*' \
+--exclude='*.css' \
+--exclude='*.js' \
+--add-header="Cache-Control: max-age=31556926"  \
+public/ s3://www.ataylor.me/
+
+# Upload CSS to S3
+echo -e "\n-> Uploading CSS to S3..."
+s3cmd \
+--access_key=$S3_ACCESS_KEY \
+--secret_key=$S3_ACCESS_SECRET \
+sync \
+--acl-public \
+--delete-removed \
+--cf-invalidate \
+--mime-type="text/css" \
+--exclude='*.*' \
+--include='*.css' \
+--add-header="Cache-Control: max-age=31556926"  \
+public/ s3://www.ataylor.me/
+
+# Upload JS to S3
+echo -e "\n-> Uploading JS to S3..."
+s3cmd \
+--access_key=$S3_ACCESS_KEY \
+--secret_key=$S3_ACCESS_SECRET \
+sync \
+--acl-public \
+--delete-removed \
+--cf-invalidate \
+--mime-type="text/javascript" \
+--exclude='*.*' \
+--include='*.js' \
+--add-header="Cache-Control: max-age=31556926"  \
+public/ s3://www.ataylor.me/
